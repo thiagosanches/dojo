40023650e480a76efad9a5af4fdf9e05de6f59f4
Thiago Sanches <thiagom@ciandt.com>
2014-03-21 15:17:03 -0300

    palindrome

---
+++ palindrome/Treinamento.Palindrome/Class1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Treinamento.Palindrome
+{
+    public class PalindromeLib
+    {
+        public Boolean verificarPalindrome(String palavra)
+        {
+            palavra = Normalizar(palavra);
+
+            int j = palavra.Length - 1;
+            for (int i = 0; i < palavra.Length; i++)
+            {
+                if (palavra[i] != palavra[j])
+                    return false;
+                j--;
+            }
+            return true;
+        }
+
+        private string Normalizar(string palavra)
+        {
+            string palavraNormalizada = palavra;
+            return palavraNormalizada.ToLower()
+                .Replace("-", "")
+                .Replace(" ", "")
+                .Replace("ô", "o")
+                .Replace(".", "")
+                .Replace("á", "a")
+                .Replace("à", "a")
+                .Replace("ã", "a")
+                .Replace("â", "a")
+                .Replace("!", "")
+                .Replace("?", "")
+                .Replace("é", "e")
+                .Replace("ê", "e")
+                .Replace("í", "i")
+                .Replace("õ", "o")
+                .Replace("ó", "o")
+                .Replace("ú", "u")
+                .Replace(",", "")
+                .Replace("(", "")
+                .Replace(")", "")
+                .Replace(";", "");
+ 
+
+        }
+        public void AbrirArquivoPalindromes(String caminho)
+        {
+            String[] linha = File.ReadAllLines(caminho);
+
+            StringBuilder saida = new StringBuilder();
+            foreach (var item in linha)
+            {
+                saida.AppendFormat(" {0} : {1}", item, verificarPalindrome(item));
+                saida.AppendLine();
+
+	        }
+            File.WriteAllText("saida.txt", saida.ToString());
+        }
+    }
+}
---
+++ palindrome/Treinamento.TestUnitario/UnitTest1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Treinamento.Palindrome;
+
+namespace Treinamento.TestUnitario
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestarAna()
+        {   
+
+            PalindromeLib palavra = new PalindromeLib();
+            Assert.IsTrue(palavra.verificarPalindrome("ana"));
+        }
+
+        [TestMethod]
+        public void TestarArara()
+        {
+            PalindromeLib palavra = new PalindromeLib();
+            Assert.IsTrue(palavra.verificarPalindrome("arara"));
+        }
+
+        [TestMethod]
+        public void TestarBanana()
+        {
+            PalindromeLib palavra = new PalindromeLib();
+            Assert.IsFalse(palavra.verificarPalindrome("banana"));
+        }
+
+        [TestMethod]
+        public void TestarFrase()
+        {
+            PalindromeLib palavra = new PalindromeLib();
+            Assert.IsTrue(palavra.verificarPalindrome("Socorram-me subi no ônibus em marrocos"));
+        }
+
+        [TestMethod]
+        public void TestarFrase2()
+        {
+            PalindromeLib palavra = new PalindromeLib();
+            Assert.IsTrue(palavra.verificarPalindrome("Aroma. Me supus em amora."));
+        }
+
+        [TestMethod]
+        public void TestarArquivo()
+           
+
+         
+        {
+            PalindromeLib ArquivoTest = new PalindromeLib();
+            ArquivoTest.AbrirArquivoPalindromes(@"C:\Users\thiagom\Downloads\lista_frases.txt");
+    
+        }
+    
+    }
+
+}
